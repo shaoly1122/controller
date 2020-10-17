4e18f5b60e00d7d1901fe22ceef6089d6034d5a7
sly <shaoly1122@vip.sina.com>
2020-10-17 21:09:45 +0800

    init commit

---
+++ src/main/java/com/dongnao/mall/controller/Logincontroller.java
@@ -0,0 +1,20 @@
+package com.dongnao.mall.controller;
+
+public class Logincontroller {
+//	public boolean login(String userName,String passWord) {
+//		LoginService loginService = new LoginService();
+//		return loginService.login(userName, passWord);
+//	}
+	
+	public void testSonar1() {
+		Long Longval = new Long(1);
+		long longval = 1L;
+		if(Longval == longval) {
+			System.out.println(1);
+		}
+		String stringal = null;
+		if(stringal.equals("123")) {
+			System.out.println(stringal);
+		}
+	}
+}
